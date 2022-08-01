fd604ec2dbe95125f0a09c7551395b7eade92170
phamduc1005 <duchungpham10052001@gmail.com>
2022-08-01 22:17:39 +0700

    fix code

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import glob
-import re
 
 dict = dict()
-list = list()
-currentFolder = ''
+typeAuthorization = ['psd', 'svg', 'json', 'jpg']
+listImages = []
 
-pathImages = glob.glob("/home/duc/createToolsWorksheetZone/Create-tools-workSheetZone/hello/*/*.jpg")
-for index, path in enumerate(pathImages):
-    pathImage = path.split('/')
-    if index == 0:
-        currentFolder = pathImage[-2]
+templates = glob.glob("/home/duc/createToolsWorksheetZone/Create-tools-workSheetZone/hello/*")
 
-    if currentFolder != pathImage[-2]:
-        dict[currentFolder] = list
-        currentFolder = pathImage[-2]
-        list.clear()
-    # findFolderParent = re.search(currentFolder, pathImage[-2])
-    else:
-        list.append(path)
-    if index == len(pathImages) - 1:
-        dict[currentFolder] = list
+for template in templates:
+    for type in typeAuthorization:
+        pathImage = '{pathImage}/*.{typeImage}'.format(pathImage = template, typeImage = type)
+        images = glob.glob(pathImage)
+        listImages += images
+
+    keyTemplate = template.split('/')[-1]
+    dict.update({ keyTemplate: listImages })
+    listImages = []
+    
 print(dict)
